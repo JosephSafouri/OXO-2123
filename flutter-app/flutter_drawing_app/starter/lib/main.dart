fd69f1d84409c73b3bd89df1d3f071605e5b3c7e
Safouri, Joseph E <jsafouri3@gatech.edu>
2022-10-07 14:40:50 -0400

    Update main.dart

--- flutter-app/flutter_drawing_app/starter/lib/main.dart
+++ flutter-app/flutter_drawing_app/starter/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:drawing_app/drawing_page.dart';
 import 'package:flutter/material.dart';
-
+///
+/// This is the main file that acts as the entry point for the app. 
+/// It contains a MyApp that contains MaterialApp. 
+/// This widget uses DrawingPage as the child.
+///
 void main() {
   runApp(MyApp());
 }
